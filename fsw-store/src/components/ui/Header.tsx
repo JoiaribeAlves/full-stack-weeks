a9211aed7c6554af8d1d8f1ffb131f9c40361d7f
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-11 02:43:03 -0400

    feat: add menu using sheet components

--- fsw-store/src/components/ui/Header.tsx
+++ fsw-store/src/components/ui/Header.tsx
@@ -1,13 +1,65 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Button } from "./shadcn/button";
 import { Card } from "./shadcn/card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./shadcn/sheet";
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between p-4 lg:p-[1.875rem]">
-      <Button size={"icon"} variant={"outline"}>
-        <MenuIcon size={16} />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size={"icon"} variant={"outline"}>
+            <MenuIcon size={16} />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side={"left"}>
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            <Button
+              variant={"outline"}
+              className="flex items-center justify-start gap-1"
+            >
+              <HomeIcon size={16} />
+              Início
+            </Button>
+
+            <Button
+              variant={"outline"}
+              className="flex items-center justify-start gap-1"
+            >
+              <LogInIcon size={16} />
+              Fazer login
+            </Button>
+
+            <Button
+              variant={"outline"}
+              className="flex items-center justify-start gap-1"
+            >
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button
+              variant={"outline"}
+              className="flex items-center justify-start gap-1"
+            >
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold lg:text-xl">
         <span className="text-primary">FSW</span> Store
