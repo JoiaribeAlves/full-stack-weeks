9ff0fdc97aa7be6e5f37c3321cbe09ce68786549
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-16 17:11:14 -0400

    feat: refactor the component

--- fsw-store/src/app/categorias/[slug]/page.tsx
+++ fsw-store/src/app/categorias/[slug]/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
 import type { Metadata } from "next";
 
 import { prismaClient } from "@/lib/prisma";
 import { computeProductTotalPrice } from "@/helpers/product";
+import { formatter } from "@/helpers/formatCurrency";
 import { GET_CATEGORY_ICON } from "@/contants/getCategoryIcon";
-import { Badge } from "@/components/ui/shadcn/badge";
-import ProductListItem from "@/components/ui/ProductListItem";
+import TitleBadge from "@/components/ui/TitleBadge";
+import DiscountBadge from "@/components/ui/DiscountBadge";
 
 export const metadata: Metadata = {
   title: "",
@@ -36,23 +39,59 @@ const Page = async ({ params }: ICategoriesSlug) => {
 
   return (
     <>
-      <div>
-        <Badge
-          className="gap-1 border-2 border-primary px-3 py-[0.375rem] text-sm uppercase lg:text-base"
-          variant={"outline"}
-        >
-          {GET_CATEGORY_ICON[params.slug as keyof typeof GET_CATEGORY_ICON]}
-          {category.name}
-        </Badge>
-      </div>
+      <TitleBadge
+        icon={GET_CATEGORY_ICON[params.slug as keyof typeof GET_CATEGORY_ICON]}
+        label={category.name}
+      />
 
-      <ul className="grid grid-cols-2 gap-4 lg:grid-cols-5">
-        {category.Products.map((product) => (
-          <ProductListItem
-            key={product.id}
-            product={computeProductTotalPrice(product)}
-            imageSize={"100%"}
-          />
+      <ul className="grid grid-cols-2 gap-x-3 gap-y-6 lg:grid-cols-5">
+        {category.Products.map((product, index) => (
+          <li key={index}>
+            <Link
+              href={`/produtos/${product.slug}`}
+              className="flex flex-col gap-3 overflow-hidden"
+            >
+              <div className="relative flex h-[200px] w-full items-center justify-center rounded-lg bg-accent">
+                <Image
+                  src={product.imgUrls[0]}
+                  alt={product.name}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ objectFit: "cover" }}
+                  className="max-h-[90%] max-w-[90%]"
+                />
+
+                {product.discountPercent > 0 && (
+                  <DiscountBadge className="absolute left-3 top-3">
+                    {product.discountPercent}
+                  </DiscountBadge>
+                )}
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <h2
+                  title={product.name}
+                  className="overflow-hidden text-ellipsis whitespace-nowrap text-lg"
+                >
+                  {product.name}
+                </h2>
+
+                <p className="text-xs line-through">
+                  de {formatter.format(Number(product.basePrice))}
+                </p>
+
+                <p className="text-sm">
+                  por R${" "}
+                  <span className="text-3xl font-medium text-primary">
+                    {formatter
+                      .format(computeProductTotalPrice(product).totalPrice)
+                      .slice(3)}
+                  </span>
+                </p>
+              </div>
+            </Link>
+          </li>
         ))}
       </ul>
     </>
