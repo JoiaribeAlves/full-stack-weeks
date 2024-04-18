5c48a1a75ec09e0c75cb51d2ec5488d5dd11c2d9
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-18 18:46:51 -0400

    feat: add page title

--- fsw-store/src/app/(home)/page.tsx
+++ fsw-store/src/app/(home)/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from "next";
+
 import Banner from "./components/Banner";
 import Categories from "./components/Categories";
 import Deals from "./components/Deals";
 import Mouses from "./components/Mouses";
 import Keyboards from "./components/Keyboards";
+
+export const metadata: Metadata = {
+  title: "FSW Store",
+};
 
 const Home = () => {
   return (
