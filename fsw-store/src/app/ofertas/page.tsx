e7307a6a40b8d78204bfb2a41c869aa102374294
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-19 01:36:08 -0400

    feat: add ProductListItem to component

--- fsw-store/src/app/ofertas/page.tsx
+++ fsw-store/src/app/ofertas/page.tsx
@@ -1,13 +1,10 @@
-import Link from "next/link";
-import Image from "next/image";
 import { PercentIcon } from "lucide-react";
 import type { Metadata } from "next";
 
 import { prismaClient } from "@/lib/prisma";
 import { computeProductTotalPrice } from "@/helpers/product";
-import { formatter } from "@/helpers/formatCurrency";
 import TitleBadge from "@/components/ui/TitleBadge";
-import DiscountBadge from "@/components/ui/DiscountBadge";
+import ProductListItem from "@/components/ui/ProductListItem";
 
 export const metadata: Metadata = {
   title: "Ofertas FSW Store",
@@ -27,58 +24,20 @@ const Page = async () => {
   });
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6 px-2 pb-24 pt-6 lg:px-8 lg:pt-8">
       <TitleBadge icon={<PercentIcon size={16} />} label="Ofertas" />
 
-      <ul className="grid grid-cols-2 gap-x-3 gap-y-6 lg:grid-cols-5">
-        {deals.map((product, index) => (
-          <li key={index}>
-            <Link
-              href={`/produtos/${product.slug}`}
-              className="flex flex-col gap-3 overflow-hidden"
-            >
-              <div className="relative flex h-[200px] w-full items-center justify-center rounded-lg bg-accent">
-                <Image
-                  src={product.imgUrls[0]}
-                  alt={product.name}
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{ objectFit: "cover" }}
-                  className="max-h-[90%] max-w-[90%]"
-                />
-
-                {product.discountPercent > 0 && (
-                  <DiscountBadge className="absolute left-3 top-3">
-                    {product.discountPercent}
-                  </DiscountBadge>
-                )}
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <h2
-                  title={product.name}
-                  className="overflow-hidden text-ellipsis whitespace-nowrap text-lg"
-                >
-                  {product.name}
-                </h2>
-
-                <p className="text-xs line-through">
-                  de {formatter.format(Number(product.basePrice))}
-                </p>
-
-                <p className="text-sm">
-                  por R${" "}
-                  <span className="text-3xl font-medium text-primary">
-                    {formatter
-                      .format(computeProductTotalPrice(product).totalPrice)
-                      .slice(3)}
-                  </span>
-                </p>
-              </div>
-            </Link>
-          </li>
-        ))}
+      <ul className="grid grid-cols-2 gap-4 lg:grid-cols-5">
+        {deals.map((deal, index) => {
+          return (
+            <li key={index} className="flex w-full">
+              <ProductListItem
+                imageSize="w-full h-[180px] lg:h-[220px]"
+                product={computeProductTotalPrice(deal)}
+              />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
