130b5a4b36fd5243254ea48df3eeefbde3e1e767
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-18 23:38:14 -0400

    feat: refactor Keyboards component

--- fsw-store/src/app/(home)/components/Keyboards.tsx
+++ fsw-store/src/app/(home)/components/Keyboards.tsx
@@ -1,8 +1,10 @@
 import { prismaClient } from "@/lib/prisma";
 import { KeyboardIcon } from "lucide-react";
 
-import ProductList from "@/components/ui/ProductList";
+import { computeProductTotalPrice } from "@/helpers/product";
 import Section from "@/components/ui/Section";
+import ProductListItem from "@/components/ui/ProductListItem";
+import { ScrollArea, ScrollBar } from "@/components/ui/shadcn/scroll-area";
 
 const Keyboards = async () => {
   const keyboards = await prismaClient.product.findMany({
@@ -15,7 +17,23 @@ const Keyboards = async () => {
 
   return (
     <Section icon={<KeyboardIcon size={16} />} label="Teclados">
-      <ProductList products={keyboards} height="h-[200px]" width="w-[200px]" />
+      <ScrollArea className="whitespace-nowrap">
+        <ul className="flex w-max gap-4 px-2 pb-4 lg:px-8">
+          {keyboards.map((keyboard, index) => {
+            return (
+              <li key={index} className="flex w-[178px] lg:w-[218px]">
+                <ProductListItem
+                  imageSize="w-[160px] h-[160px] lg:h-[200px] lg:w-[200px]"
+                  product={computeProductTotalPrice(keyboard)}
+                  priceSize="text-2xl"
+                />
+              </li>
+            );
+          })}
+        </ul>
+
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </Section>
   );
 };
