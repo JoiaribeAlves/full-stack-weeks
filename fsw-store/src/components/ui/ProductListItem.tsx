1926a2f86b4107084fefca001c2c01d33fe15f11
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-19 19:18:58 -0400

    feat: allows the user to add the product directly to the cart

--- fsw-store/src/components/ui/ProductListItem.tsx
+++ fsw-store/src/components/ui/ProductListItem.tsx
@@ -1,10 +1,17 @@
+"use client";
+
+import { useContext } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { CheckIcon, ShoppingCartIcon } from "lucide-react";
 import { twMerge } from "tailwind-merge";
+import { toast } from "sonner";
 
+import { CartContext } from "@/providers/cart";
 import { IComputeProductTotalPrice } from "@/helpers/product";
 import { formatter } from "@/helpers/formatCurrency";
 import DiscountBadge from "./DiscountBadge";
+import { Button } from "./shadcn/button";
 
 interface IProductListItem {
   product: IComputeProductTotalPrice;
@@ -17,11 +24,23 @@ const ProductListItem = ({
   imageSize,
   priceSize,
 }: IProductListItem) => {
+  const { addProductToCart } = useContext(CartContext);
+
+  const handleAddProductToCart = () => {
+    addProductToCart({ ...product, productQuantity: 1 });
+
+    toast(`${product.name} foi adicionado ao seu carrinho`, {
+      duration: 3500,
+      position: "top-right",
+      icon: <CheckIcon size={14} />,
+    });
+  };
+
   return (
-    <div className="flex w-full">
+    <div className="flex w-full flex-col gap-3 rounded-lg border border-accent p-2">
       <Link
         href={`/produtos/${product.slug}`}
-        className="flex w-full flex-col gap-3 rounded-lg border border-accent p-2"
+        className="flex w-full flex-grow flex-col gap-3"
       >
         <div
           className={twMerge(
@@ -76,6 +95,15 @@ const ProductListItem = ({
           )}
         </div>
       </Link>
+
+      <Button
+        size="sm"
+        variant="secondary"
+        className="flex items-center gap-1 text-xs uppercase hover:bg-primary hover:text-primary-foreground"
+        onClick={handleAddProductToCart}
+      >
+        <ShoppingCartIcon size={16} /> Adicionar ao carrinho
+      </Button>
     </div>
   );
 };
